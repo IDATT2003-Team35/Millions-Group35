b1ce339559c033d1127b317ceb5ee49a44813ebc
Aksel Kirkhorn <akseltk@stud.ntnu.no>
2026-02-06 15:34:56 +0100

    feat: add Purchase class for transaction handling

---
+++ src/main/java/edu/ntnu/idi/idatt/millions/transaction/Purchase.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idi.idatt.millions.transaction;
+
+import edu.ntnu.idi.idatt.millions.calculator.PurchaseCalculator;
+import edu.ntnu.idi.idatt.millions.calculator.Share;
+
+public class Purchase extends Transaction{
+
+  public Purchase(Share share, int week) {
+    super(share, week, new PurchaseCalculator(share));
+  }
+
+  @Override
+  protected void executeTransaction(Player player) {
+
+  }
+}
